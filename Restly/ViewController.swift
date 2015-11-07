85d4d1c8a6ad37c235f6e8d8067194282dfb47f4
Alan Stephensen <me@alan.io>
2015-11-08 09:25:43 +1000

    Initial Commit

---
+++ Restly/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Restly
+//
+//  Created by Alan Stephensen on 8/11/2015.
+//  Copyright © 2015 Alan Stephensen. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
