be09e1a1651aee99025e1831756385b999828019
Alan Stephensen <me@alan.io>
2015-12-05 11:41:52 +1000

    View structuring

---
+++ Restly/Controllers/MainWindowController.swift
@@ -0,0 +1,20 @@
+//
+//  MainWindowController.swift
+//  Restly
+//
+//  Created by Alan Stephensen on 5/12/2015.
+//  Copyright © 2015 Alan Stephensen. All rights reserved.
+//
+
+import Cocoa
+
+class MainWindowController: NSWindowController {
+
+    override func windowDidLoad() {
+        super.windowDidLoad()
+    
+        // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+        window?.titleVisibility = .Hidden
+    }
+
+}
