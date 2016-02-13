f502b94d4f2a8e15bfefdf722fa0085bc0c0f95e
Alan Stephensen <me@alan.io>
2016-02-13 21:56:22 +1000

    Improving syntax highlighting

--- Restly/Vendor/CodeMirror/CodeMirror.bundle/mode/restful/restful.js
+++ Restly/Vendor/CodeMirror/CodeMirror.bundle/mode/restful/restful.js
@@ -8,32 +8,75 @@
 })(function(CodeMirror) {
 "use strict";
 
-  var methodRegex = /(?:GET|PUT|POST|PATCH|DELETE|HEAD|OPTIONS|TRACE|CONNECT)\b/;
+var restfulMode = CodeMirror.defineMode("restful", function (config) {
 
-  CodeMirror.defineSimpleMode("restful", {
-    start: [
-      // Methods.
-      {
-        regex: methodRegex,
-        token: "keyword",
-        next: "method"
+  var methods = {
+    "GET": true, "PUT": true, "POST": true, "PATCH": true, "DELETE": true, "HEAD": true, "OPTIONS": true,
+    "TRACE": true, "CONNECT": true
+  };
+
+  function tokenBase(stream, state) {
+    var ch = stream.next();
+
+    // JSON.
+    if (state.startOfLine && (ch == "{" || ch == "[")) {
+      var mode = CodeMirror.getMode({json: true}, "javascript")
+      state.token = function (stream, state) {
+        // Check for closing brace.
+        var nextChar = stream.peek();
+        if (stream.sol() && (nextChar == "}" || nextChar == "]")) {
+          state.token = tokenBase;
+          stream.next();
+          return "comment";
+        }
+        var ch = stream.next();
+        return state.localMode.token(stream, state.localState);
       }
-    ],
-    // Method state.
-    method: [
-      {
-        regex: /.*/,
-        token: "text",
-        next: "start"
-      }
-    ],
-    // The meta property contains global information about the mode. It
-    // can contain properties like lineComment, which are supported by
-    // all modes, and also directives like dontIndentStates, which are
-    // specific to simple modes.
-    meta: {
-      lineComment: "//"
+      state.localMode = mode;
+      state.localState = CodeMirror.startState(mode);
+      return "comment";
     }
-  });
+
+    // Methods.
+    stream.eatWhile(/[\w]/);
+    var cur = stream.current();
+    if (methods.propertyIsEnumerable(cur)) {
+     return "keyword";
+    }
+
+    return null;
+  }
+
+  return {
+    startState: function() {
+      return {
+        token: tokenBase,
+        localMode: null,
+        localState: null,
+        startOfLine: true
+      }
+    },
+
+    copyState: function (state) {
+      console.log('COPY STATE!');
+      var local;
+      if (state.localState) {
+        local = CodeMirror.copyState(state.localMode, state.localState);
+      }
+      return {
+        token: state.token,
+        localMode: state.localMode,
+        localState: local,
+      };
+    },
+
+    token: function (stream, state) {
+      state.startOfLine = stream.sol();
+      return state.token(stream, state);
+    }
+  };
+});
+
+CodeMirror.defineMIME("text/restful", "restful");
 
 });
